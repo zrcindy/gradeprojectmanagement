f34953861799fa6104f1e669cb8a118090b708e6
root <root@localhost.localdomain>
2024-05-26 20:06:33 -0500

    Adding Users table to DB initialization

--- mysql-init/db_seed.sql
+++ mysql-init/db_seed.sql
@@ -2,6 +2,13 @@ CREATE DATABASE database_production;
 USE database_production;
 GRANT ALL PRIVILEGES ON database_production.* TO 'userdba'@'%';
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    role ENUM('Estudiante', 'Evaluador') NOT NULL
+);
+
 CREATE TABLE propuestas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
